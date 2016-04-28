02aa46491da10004cbae7797936ba23b1c8b75c7
Carlos <carlosjesus94@gmail.com>
2016-04-28 14:49:19 -0430

    Add footer header and overall layout

--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -13,14 +13,17 @@ class Summoner < ActiveRecord::Base
     api = RiotApiRequests.new
 
     summoner_data = api.request_summoner_data(name, summoner.region).first.last
+    champion_stats_data = api.request_champion_ranked_stats_data(summoner.summoner_id, region)
+
     summoner.logo_id = summoner_data["profileIconId"]
     summoner.champion_masteries.delete_all
+    summoner.champion_stats.delete_all
 
     summoner.save(:validate => false)
 
     mastery_data = api.request_mastery_data(summoner.summoner_id, summoner.region)
 
-    inserts = []
+    masteries = []
     mastery_data.each do |mastery|
       if mastery.has_key?("highestGrade")
         grade = mastery["highestGrade"]
@@ -33,12 +36,31 @@ class Summoner < ActiveRecord::Base
       chest_granted = mastery["chestGranted"]
       champion_id = mastery["championId"]
 
-      inserts.push "(#{champion_points}, 'f', '#{grade}', #{champion_id}, #{summoner.id}, '#{Time.current.to_s}', '#{Time.current.to_s}')"
+      masteries.push "(#{champion_points}, 'f', '#{grade}', #{champion_id}, #{summoner.id}, '#{Time.current.to_s}', '#{Time.current.to_s}')"
     end
 
-    sql = "INSERT INTO champion_masteries (champion_points, chest_granted, highest_grade, champion_id, summoner_id, created_at, updated_at) VALUES #{inserts.join(", ")};"
+    champions = []
+    unless champion_stats_data.nil? 
+      champion_stats_data["champions"].each do |stat|
+        champion_id = stat["id"]
+        matches_played = stat["stats"]["totalSessionsPlayed"]
+        matches_won = stat["stats"]["totalSessionsWon"]
+        matches_lost = stat["stats"]["totalSessionsLost"]
+        kills = stat["stats"]["totalChampionKills"]
+        assists = stat["stats"]["totalAssists"]
+        deaths = stat["stats"]["totalDeathsPerSession"]
+        current_time = Time.current.to_s
+        champions.push "(#{matches_played}, #{matches_won}, #{matches_lost}, #{kills}, #{assists}, #{deaths}, #{champion_id}, #{summoner.id}, '#{current_time}', '#{current_time}')"
+      end
+    end
 
-    ChampionMastery.connection.execute sql
+    champion_masteries_query = "INSERT INTO champion_masteries (champion_points, chest_granted, highest_grade, champion_id, summoner_id, created_at, updated_at) VALUES #{masteries.join(", ")};"
+    champion_stats_query = "INSERT INTO champion_stats (matches_played, matches_won, matches_lost, kills, assists, deaths, champion_id, summoner_id, created_at, updated_at) VALUES #{champions.join(", ")};"
+
+    ActiveRecord::Base.transaction do
+      ChampionMastery.connection.execute champion_masteries_query
+      ChampionStat.connection.execute champion_stats_query unless champion_stats_data.nil?
+    end
 
     summoner
   end
