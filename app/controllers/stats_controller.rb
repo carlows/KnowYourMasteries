2ada9a39d8f3e7906e9d3407216a0587a94c1a83
Carlos <carlosjesus94@gmail.com>
2016-05-07 07:24:26 -0430

    table comparison

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -4,5 +4,7 @@ class StatsController < ApplicationController
   def show
     @champion_mains = Champion.joins(:summoners).group('key').count
     @chests_unlocked = ChampionMastery.where(chest_granted: true).count
+    @champions_data = Champion.includes(:champion_masteries, :champion_stats).all.as_json(methods: [:mastery_points_average, :winrate_average])
+    @champion_comparison = Champion.all.map { |champ| [champ.mastery_points_average.to_i, champ.winrate_average.to_i, champ.name] }
   end
 end
--- app/models/champion.rb
+++ app/models/champion.rb
@@ -3,5 +3,13 @@ class Champion < ActiveRecord::Base
   has_many :champion_stats
   has_many :summoners
 
+  def mastery_points_average
+    champion_masteries.average(:champion_points).to_i
+  end
+
+  def winrate_average
+    champion_stats.average('((matches_won::float / matches_played::float) * 100)::integer').to_i
+  end
+
   scope :by_summoner, -> (summoner_id) { where("champion_masteries.summoner_id = ? AND champion_stats.summoner_id = ?", summoner_id, summoner_id) }
 end
