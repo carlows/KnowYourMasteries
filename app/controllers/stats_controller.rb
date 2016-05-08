95a84778dcce371c49bb29006c39dc21529591e8
Carlos <carlosjesus94@gmail.com>
2016-05-08 02:15:12 -0430

    replace table by a chart

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -17,5 +17,17 @@ class StatsController < ApplicationController
         'data' => [{ 'x' => champ.mastery_points_average.to_i, 'y' => champ.winrate_average.to_i, 'marker' => { 'width' => 20, 'height' => 20, 'symbol' => "url(http://ddragon.leagueoflegends.com/cdn/6.9.1/img/champion/#{champ.key}.png)" } }]
       }
     }
+
+    alphabet = 'SABCDEFGHIJKLMOPQRTUVWXYZNn'
+
+    custom_sort = ->(a, b) do
+      a.split('').each_with_index do |char, i|
+        return alphabet.index(a[i]) <=> alphabet.index(b[i]) if a[i] != b[i]
+      end
+
+      return alphabet.index(' ') <=> alphabet.index(b[-1])
+    end
+
+    @mastery_grades = ChampionMastery.all.group_by{ |mastery| mastery.highest_grade[0] }.map { |key, value| [ key, value.count ] if key != 'n' }.compact.sort(&custom_sort).to_h
   end
 end
